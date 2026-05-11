0743a10dcc3a24137dd0d48652c1c013c35874c1
andy henrie <ergonyc@gmail.com>
2026-05-11 12:23:38 -0700

    add updates

--- scripts/fix_collections_v.4.0.1_release.py
+++ scripts/fix_collections_v.4.0.1_release.py
@@ -35,15 +35,126 @@ for collection in collection_names:
                 json.dump(ds_info, f, indent=4)
 
             # also save it to the archive
+            ds_info["version"] = ds_version
+            # fix releases to only note if the daset was "new"
+            # load release.json
+            test_releases = ds_info["releases"].copy()
+            for rel_ver,info in test_releases.items():
+                if rel_ver <= "v4.0.1":
+                    with open(releases_repo_path / rel_ver / "release.json", "r") as f:
+                        rel_info = json.load(f)
+                    new_datasets = [x["name"] for x in rel_info["new_datasets"] ]
+                    if ds not in new_datasets:
+                        ds_info["releases"].pop(rel_ver)
+            
             with open(ds_path / "archive" / ds_version / "dataset.json", "w") as f:
                 json.dump(ds_info, f, indent=4)
-
 
 # %%
 
 with open(datasets_repo_path / "datasets.json", "r") as f:
     datasets = json.load(f)
 
+# get all the datasets to check
+
+for ds, _ds_info in datasets.items():
+    print(f"processing {ds}")
+    ds_path = datasets_repo_path / "datasets" / ds
+    with open(ds_path / "dataset.json", "r") as f:
+        ds_info = json.load(f)
+
+
+    # test_releases = ds_info["releases"].copy()
+    # # make sure that we only keep "new_release" entries..
+    # for rel_ver,info in test_releases.items():
+    #     if rel_ver <= "v4.0.1":
+    #         with open(releases_repo_path / rel_ver / "release.json", "r") as f:
+    #             rel_info = json.load(f)
+    #         new_datasets = [x["name"] for x in rel_info["new_datasets"] ]
+    #         if ds not in new_datasets:
+    #             # print(f"(new) popping {ds_info["releases"][rel_ver]=}")
+    #             ds_info["releases"].pop(rel_ver)
+
+    # # save updated info root datset.json
+    # with open(ds_path / "dataset.json", "w") as f:
+    #     json.dump(ds_info, f, indent=4)
+
+    # #reload
+    # with open(ds_path / "dataset.json", "r") as f:
+    #     ds_info = json.load(f)
+    # print(f"{ds_info['releases']}")
+    ds_info_copy = ds_info.copy() # start over fore each version...
+
+
+    # now we need to check all of our versions....
+    ds_vers = [info["dataset_version"] for rel_ver,info in ds_info["releases"].items()]
+    ds_vers = sorted(list(set(ds_vers)), reverse=True)
+    ds_info_releases = ds_info["releases"].copy()
+    print(f"all versions = {ds_vers=}")
+
+
+    for ds_ver in ds_vers: 
+        # force this probe version
+        _ds_info = ds_info_copy.copy()
+        _ds_info["version"] = ds_ver
+
+        print(f"DATASET version: {ds_ver}, {_ds_info['releases']=}")
+        # just make sure we have all of the ds_ver
+
+
+        # # need to make sure to get the higest release info for each version.
+        # for rel_ver, info in ds_info_releases.items():
+        #     if rel_ver <= "v4.0.1":
+        #         with open(releases_repo_path / rel_ver / "release.json", "r") as f:
+        #             rel_info = json.load(f)
+        #         new_datasets_ver = {x["name"]:x["version"] for x in rel_info["new_datasets"] }
+
+        #         if ds not in set(new_datasets_ver.keys()):
+        #             # print(f"(new) popping {ds_info["releases"][rel_ver]=}")
+        #             ds_info["releases"].pop(rel_ver)
+
+        curr_ds_info_rel = _ds_info["releases"].copy()
+
+        for rel_ver, info in curr_ds_info_rel.items():
+            # start with new ds_info every time...
+            # with open(ds_path / "dataset.json", "r") as f:
+            #     ds_info_now = json.load(f)  
+            ds_info_now = _ds_info.copy()
+            # ds_info_now["version"] = ds_ver
+            # now make sure we don't have any above ds_ver
+            # now find the pop any releases where the ds_ver doesn't match
+            print(f"dataset_version={info["dataset_version"]},{ds_ver=}")
+            if info["dataset_version"] > ds_ver:
+                print(f"(ds) ds {ds_ver=} popping {rel_ver} from {ds_info_now['releases']}")
+                popped = ds_info_now["releases"].pop(rel_ver)
+                print(f"{popped=}")
+
+        ver_path = ds_path / "archive" / ds_ver 
+        if not ver_path.exists():
+            ver_path.mkdir(parents=True)
+
+        with open(ds_path / "archive" / ds_ver / "dataset.json", "w") as f:
+            json.dump(ds_info_now, f, indent=4)
+
+
+# # %%
+#     # also save it to the archive
+#     # fix releases to only note if the daset was "new"
+#     # load release.json
+#     test_releases = ds_info["releases"].copy()
+#     for rel_ver,info in test_releases.items():
+#         if rel_ver <= "v4.0.1":
+#             with open(releases_repo_path / rel_ver / "release.json", "r") as f:
+#                 rel_info = json.load(f)
+#             new_datasets = [x["name"] for x in rel_info["new_datasets"] ]
+#             if ds not in new_datasets:
+#                 ds_info["releases"].pop(rel_ver)
+    
+
+
+
+
+# %%
 
 all_datasets = list(datasets.keys())
 datasets_dict = {}
@@ -68,7 +179,7 @@ for dataset, ds_info in datasets.items():
     releases = []
     for rel_ver, rel_info in all_versions.items():
         releases.append(rel_ver)
-    ds_vers.append(rel_info["dataset_version"])
+        ds_vers.append(rel_info["dataset_version"])
 
     dataset_model = ao.Dataset.load(ds_path)
     export_to_datasets = dataset_model.model_dump()
@@ -84,4 +195,5 @@ with open(datasets_json_path, "w") as f:
 
 
 
+# %
 # %%
